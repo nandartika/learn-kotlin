10d1ee8fe7274eb5c941312ee9ab5a278f20d06f
cst_dev170 <cst_dev170@unitedtractors.com>
2023-07-12 13:03:35 +0700

    refactor(data-binding): implementated data binding in BindingDemo2

--- BindingDemo2/app/src/main/java/com/anushka/bindingdemo2/MainActivity.kt
+++ BindingDemo2/app/src/main/java/com/anushka/bindingdemo2/MainActivity.kt
@@ -1,30 +1,33 @@
 package com.anushka.bindingdemo2
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.ProgressBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.anushka.bindingdemo2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var button: Button
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        button = findViewById(R.id.control_button)
-        button.setOnClickListener {
-            startOrStopProgressBar()
-        }
-    }
+	private lateinit var button: Button
+	private lateinit var binding: ActivityMainBinding
 
-    private fun startOrStopProgressBar() {
-        val progressBar = findViewById<ProgressBar>(R.id.progressBar)
-        if (progressBar.visibility == View.GONE) {
-            progressBar.visibility = View.VISIBLE
-            button.text = "Stop"
-        } else {
-            progressBar.visibility = View.GONE
-            button.text = "Start"
-        }
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+		binding.controlButton.setOnClickListener {
+			startOrStopProgressBar()
+		}
+	}
+
+	private fun startOrStopProgressBar() {
+		binding.apply {
+			if (progressBar.visibility == View.GONE) {
+				progressBar.visibility = View.VISIBLE
+				controlButton.text = "Stop"
+			} else {
+				progressBar.visibility = View.GONE
+                controlButton.text = "Start"
+			}
+		}
+	}
 }
