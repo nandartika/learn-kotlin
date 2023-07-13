1a403c8719bf66451d554710d41a79052634d0c8
cst_dev170 <cst_dev170@unitedtractors.com>
2023-07-13 12:58:01 +0700

    refactor(live-data): implementated live data in ViewModelChallenge

--- ViewModelDemo2/app/src/main/java/com/anushka/viewmodeldemo2/MainActivityViewModel.kt
+++ ViewModelDemo2/app/src/main/java/com/anushka/viewmodeldemo2/MainActivityViewModel.kt
@@ -1,19 +1,19 @@
 package com.anushka.viewmodeldemo2
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class MainActivityViewModel(startingTotal: Int) : ViewModel() {
-    private var total = 0
+	private var total = MutableLiveData<Int>()
+	val totalData: LiveData<Int>
+		get() = total
 
-    init {
-    	total = startingTotal
-    }
+	init {
+		total.value = startingTotal
+	}
 
-    fun getTotal():Int{
-        return total
-    }
-
-    fun setTotal(input:Int){
-        total +=input
-    }
+	fun setTotal(input: Int) {
+		total.value = (totalData.value)?.plus(input)
+	}
 }
