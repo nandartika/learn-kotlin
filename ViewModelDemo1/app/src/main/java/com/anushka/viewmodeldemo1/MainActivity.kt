8f3812911ebaa01debdc80d45cfed564d6d1311d
cst_dev170 <cst_dev170@unitedtractors.com>
2023-07-13 13:56:32 +0700

    refactor(live-data): implementated live data in ViewModelDemo1

--- ViewModelDemo1/app/src/main/java/com/anushka/viewmodeldemo1/MainActivity.kt
+++ ViewModelDemo1/app/src/main/java/com/anushka/viewmodeldemo1/MainActivity.kt
@@ -1,20 +1,23 @@
 package com.anushka.viewmodeldemo1
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.anushka.viewmodeldemo1.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+	private lateinit var binding: ActivityMainBinding
+	private lateinit var viewModel: MainActivityViewModel
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        binding.countText.text = count.toString()
-        binding.button.setOnClickListener {
-            count++
-            binding.countText.text = count.toString()
-        }
-    }
+	override fun onCreate(savedInstanceState: Bundle?) {
+		super.onCreate(savedInstanceState)
+		viewModel = ViewModelProvider(this).get(MainActivityViewModel::class.java)
+		binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+		viewModel.countData.observe(this, Observer { binding.countText.text = it.toString() })
+		binding.button.setOnClickListener {
+			viewModel.updateCount()
+		}
+	}
 }
