78d3341c1797b866694529491104033305a396ed
nandartika <kartikanandap@gmail.com>
2023-09-11 10:56:07 +0700

    initial commit NavDemo1

---
+++ NavDemo1/app/src/main/java/com/anushka/navdemo1/SecondFragment.kt
@@ -0,0 +1,24 @@
+package com.anushka.navdemo1
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import com.anushka.navdemo1.databinding.FragmentSecondBinding
+
+class SecondFragment : Fragment() {
+    private lateinit var binding: FragmentSecondBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false)
+        val input = requireArguments().getString("user_input")
+        binding.textView.text = input
+        return binding.root
+    }
+}
